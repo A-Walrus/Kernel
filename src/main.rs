d5c6804ffdc57a9385dde2b68ca5e8f548d58b28
Awalrus <ofek.r.guy@gmail.com>
2021-10-10 17:35:53 +0300

    Added bitmap font in font.rs
    The font is font_sun8x16 from the linux kernel

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,17 @@
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 
+mod IO;
+
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
-	// turn the screen gray
 	if let Some(framebuffer) = boot_info.framebuffer.as_mut() {
 		let other_buffer: &[u8] = &[0xff; 1921024];
 
 		framebuffer.buffer_mut().copy_from_slice(other_buffer);
 	}
+	let x = IO::font::FONT;
 
 	loop {}
 }

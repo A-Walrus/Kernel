d1d5b341ac2822c8026dbfc23b921889ec7c29b9
Awalrus <ofek.r.guy@gmail.com>
2021-10-11 12:09:44 +0300

    Add groundwork for pixel buffers

---
+++ src/io/buffer.rs
@@ -0,0 +1,9 @@
+#[repr(C)]
+#[repr(packed)]
+#[derive(Copy, Clone)]
+pub struct Pixel {
+	pub blue: u8,
+	pub green: u8,
+	pub red: u8,
+	pub unknown: u8,
+}
--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,29 @@
 #![no_main]
 
 use bootloader::{entry_point, BootInfo};
-use core::panic::PanicInfo;
+use core::{mem::transmute, panic::PanicInfo};
 
-mod IO;
+mod io;
+use io::buffer::Pixel;
 
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
 	if let Some(framebuffer) = boot_info.framebuffer.as_mut() {
-		let other_buffer: &[u8] = &[0xff; 1921024];
-
-		framebuffer.buffer_mut().copy_from_slice(other_buffer);
+		let buffer = framebuffer.buffer_mut();
+		// let back: &[Pixel] = &[Pixel {
+		// 	red: 0xff,
+		// 	green: 0,
+		// 	blue: 0,
+		// 	unknown: 0xff,
+		// }; 480256];
+		// unsafe {
+		// 	let front = buffer as *mut [u8];
+		// 	let front = front as *mut [Pixel];
+		// 	let front = &mut *front;
+		// 	front.copy_from_slice(back);
+		// }
 	}
-	let x = IO::font::FONT;
 
 	loop {}
 }

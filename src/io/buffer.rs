5308bc56747c16da9263ddc94c42426d411974fe
Awalrus <ofek.r.guy@gmail.com>
2021-10-12 19:45:42 +0300

    Add basic screen buffers (double buffered)

--- src/io/buffer.rs
+++ src/io/buffer.rs
@@ -1,3 +1,5 @@
+pub const SCREEN_SIZE: usize = 480256;
+
 #[repr(align(4))]
 #[derive(Copy, Clone)]
 pub struct Pixel {
@@ -10,4 +12,35 @@ impl Pixel {
 	pub fn new(red: u8, green: u8, blue: u8) -> Self {
 		Pixel { blue, green, red }
 	}
+}
+
+type Buffer<'a> = &'a mut [Pixel];
+
+struct ScreenInfo {}
+
+pub struct Screen<'a> {
+	pub front: Buffer<'a>,
+	pub back: Buffer<'a>,
+	info: ScreenInfo,
+}
+
+impl<'a> Screen<'a> {
+	pub fn new(front: Buffer<'a>, back: Buffer<'a>) -> Self {
+		Screen {
+			front,
+			back,
+			info: ScreenInfo {},
+		}
+	}
+
+	pub fn flush(&mut self) {
+		self.front.copy_from_slice(self.back);
+	}
+}
+
+#[macro_export]
+macro_rules! as_pixels {
+	($buf:expr) => {
+		unsafe { &mut *(($buf as *mut [u8]) as *mut [Pixel; SCREEN_SIZE]) }
+	};
 }
--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,21 @@ use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 
 mod io;
-use io::{buffer::Pixel, serial};
+use crate::buffer::SCREEN_SIZE;
+use io::{
+	buffer::{self, Pixel, Screen},
+	serial,
+};
 
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
 	if let Some(framebuffer) = boot_info.framebuffer.as_mut() {
 		let buffer = framebuffer.buffer_mut();
-		let pixel = Pixel::new(0, 0, 0xff);
-		let back: &[Pixel] = &[pixel; 480256];
-		unsafe {
-			let front = buffer as *mut [u8];
-			let front = front as *mut [Pixel; 480256];
-			let front = &mut *front;
-			front.copy_from_slice(back);
-		}
+		let pixel = Pixel::new(0, 0, 0x0);
+		let back: &mut [Pixel] = &mut [pixel; buffer::SCREEN_SIZE];
+		let mut screen = Screen::new(as_pixels!(framebuffer.buffer_mut()), back);
+		screen.flush();
 	}
 
 	loop {}

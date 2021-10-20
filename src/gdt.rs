dfbdedc0ca03de2d699f37240d0fe8800c883129
Awalrus <ofek.r.guy@gmail.com>
2021-10-20 12:30:52 +0300

    Get sector selectors and set CS selector

--- src/gdt.rs
+++ src/gdt.rs
@@ -1,22 +1,37 @@
 use lazy_static::lazy_static;
 use x86_64::{
 	self,
-	structures::gdt::{Descriptor, GlobalDescriptorTable},
+	instructions::segmentation::*,
+	structures::gdt::{Descriptor, GlobalDescriptorTable, SegmentSelector},
 };
-
 lazy_static! {
-	static ref GDT: GlobalDescriptorTable = {
+	static ref GDT: (GlobalDescriptorTable, Selectors) = {
 		let mut gdt = GlobalDescriptorTable::new();
-		let kernel_code_segment = gdt.add_entry(Descriptor::kernel_code_segment());
-		let kernel_data_segment = gdt.add_entry(Descriptor::kernel_data_segment());
-		let user_code_segment = gdt.add_entry(Descriptor::user_data_segment());
-		let user_data_segment = gdt.add_entry(Descriptor::user_data_segment());
-		gdt
+		let kernel_code_selector = gdt.add_entry(Descriptor::kernel_code_segment());
+		let kernel_data_selector = gdt.add_entry(Descriptor::kernel_data_segment());
+		let user_code_selector = gdt.add_entry(Descriptor::user_data_segment());
+		let user_data_selector = gdt.add_entry(Descriptor::user_data_segment());
+		(
+			gdt,
+			Selectors {
+				kernel_code_selector,
+				kernel_data_selector,
+				user_code_selector,
+				user_data_selector,
+			},
+		)
 	};
+}
+struct Selectors {
+	kernel_code_selector: SegmentSelector,
+	kernel_data_selector: SegmentSelector,
+	user_code_selector: SegmentSelector,
+	user_data_selector: SegmentSelector,
 }
 
 pub fn setup() {
 	unsafe {
-		GDT.load();
+		GDT.0.load();
+		set_cs(GDT.1.kernel_code_selector);
 	}
 }

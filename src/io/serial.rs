0b0b390c265a510ce586efc785be9a516e9724aa
Awalrus <ofek.r.guy@gmail.com>
2021-10-28 18:08:10 +0300

    Remove semicolon from macro

--- src/io/serial.rs
+++ src/io/serial.rs
@@ -22,7 +22,7 @@ pub fn _print(args: ::core::fmt::Arguments) {
 #[macro_export]
 macro_rules! serial_print {
     ($($arg:tt)*) => {
-        $crate::serial::_print(format_args!($($arg)*));
+        $crate::serial::_print(format_args!($($arg)*))
     };
 }
 
@@ -32,5 +32,5 @@ macro_rules! serial_println {
     () => ($crate::serial_print!("\n"));
     ($fmt:expr) => ($crate::serial_print!(concat!($fmt, "\n")));
     ($fmt:expr, $($arg:tt)*) => ($crate::serial_print!(
-        concat!($fmt, "\n"), $($arg)*));
+        concat!($fmt, "\n"), $($arg)*))
 }

16df57db9627adf3cf526cbb6d459e50f0350aa6
Awalrus <ofek.r.guy@gmail.com>
2021-11-20 12:35:56 +0200

    Add documentation

--- src/mem/heap.rs
+++ src/mem/heap.rs
@@ -1,5 +1,6 @@
 extern crate alloc;
 
+/// The static global allocator struct.
 #[global_allocator]
 static ALLOCATOR: Locked<BuddyAllocator> = Locked::new(BuddyAllocator::new());
 
@@ -8,13 +9,21 @@ use alloc::alloc::{GlobalAlloc, Layout};
 use bootloader::boot_info::MemoryRegions;
 use core::{cmp::max, mem::size_of, ptr::null_mut};
 
+/// The log of the size of the heap (the size of the heap must be a power of 2)
 const LOG_HEAP_SIZE: usize = 23; // 8MB
+/// The actual size of the heap (2 <sup> [LOG_HEAP_SIZE] </sup>)
 const HEAP_SIZE: usize = 1 << LOG_HEAP_SIZE;
+/// The starting (virtual) address of the heap.
 const HEAP_START: usize = 0xFFFF980000000000;
 
+/// The log of the size of the smallest block (the sizes of all the blocks are powers of two.
 const LOG_SMALLEST_SIZE: usize = 5;
+/// The size of the smallest block (2 <sup> [LOG_SMALLEST_SIZE] </sup>).
+/// This value must be big enough so that a [Node] will fit in it.
 const SMALLEST_SIZE: usize = 1 << LOG_SMALLEST_SIZE;
+/// The number of layers (block sizes) in this buddy allocator heap.
 const LAYERS: usize = LOG_HEAP_SIZE - LOG_SMALLEST_SIZE;
+/// Array of the size of the blocks in each layer.
 const SIZES: [usize; LAYERS] = {
 	let mut size = HEAP_SIZE;
 	let mut layers = [0; LAYERS];
@@ -50,14 +59,19 @@ pub fn setup(memory_regions: &'static MemoryRegions) {
 	ALLOCATOR.lock().init();
 }
 
+/// The connection between nodes in a doubly linked list
 type Link = Option<usize>;
 
+/// A node in a doubly linked list.
 #[derive(Debug, Copy, Clone)]
 struct Node {
-	next: Link,
+	/// A link to the previous node.
 	prev: Link,
+	/// A link to the next node.
+	next: Link,
 }
 
+/// The number of pairs of buddys (2 <sup> [LAYERS] </sup> - 1).
 const BUDDY_PAIRS: usize = (1 << LAYERS) - 1;
 
 /// Struct representing the global allocator. It implements [GlobalAlloc] and is
@@ -68,11 +82,12 @@ pub struct BuddyAllocator {
 	// TODO store each value as a single bit, and not a full byte.
 	xor_free: [bool; BUDDY_PAIRS],
 
-	/// linked list for every size
+	/// linked list of free blocks for every layer.
 	linked_lists: [Node; LAYERS],
 }
 
 impl BuddyAllocator {
+	/// Constructor for a [BuddyAllocator]. **Does not add any free blocks to any list**.
 	const fn new() -> Self {
 		let empty = Node { next: None, prev: None };
 		BuddyAllocator {
@@ -81,30 +96,36 @@ impl BuddyAllocator {
 		}
 	}
 
+	/// Add the entire heap as a free block of layer 0.
 	fn init(&mut self) {
 		self.add_free_block(0, false)
 	}
 
 	/// returns index into [BuddyAllocator::linked_lists], which holds the smallest blocks big
-	/// enough to store something of the ```wanted_size```
+	/// enough to store something of the ```wanted_size```.
 	fn layer_from_size(wanted_size: usize) -> usize {
 		let log = wanted_size.log2() as usize;
 		LAYERS + LOG_SMALLEST_SIZE - 1 - max(log, LOG_SMALLEST_SIZE)
 	}
 
-	fn get_id_in_layer(layer: usize, addr: usize) -> usize {
+	/// Get the index of a block of a certain address in a certain layer.
+	fn get_index_in_layer(layer: usize, addr: usize) -> usize {
 		(addr - HEAP_START) / SIZES[layer]
 	}
 
+	/// Get the id of a block by its layer and address.
 	fn get_id(layer: usize, addr: usize) -> usize {
 		let start_of_layer = (1 << layer) - 1;
-		BuddyAllocator::get_id_in_layer(layer, addr) + start_of_layer
+		BuddyAllocator::get_index_in_layer(layer, addr) + start_of_layer
 	}
 
+	/// Get the id of a blocks buddy.
 	fn get_buddy_id(id: usize) -> usize {
 		id + ((id % 2) * 2) - 1
 	}
 
+	/// Get a pointer to a block from its id. Can be returned as result of allocation request, or
+	/// cast into a [Node] to edit linked list.
 	fn id_to_ptr(id: usize) -> *mut u8 {
 		let addr = HEAP_START;
 		let layer = BuddyAllocator::layer_from_id(id);
@@ -122,19 +143,25 @@ impl BuddyAllocator {
 		(id - 1) / 2
 	}
 
+	/// Get the layer a certain id belongs to.
 	fn layer_from_id(id: usize) -> usize {
 		((id + 1).log2()) as usize
 	}
 
+	/// Get the position of a block with a certain id, inside its layer.
 	fn pos_in_layer(id: usize, layer: usize) -> usize {
 		let start_of_layer = (1 << layer) - 1;
 		id - start_of_layer
 	}
 
+	/// Get the ids of the children of a certain block.
 	fn get_children_ids(id: usize) -> [usize; 2] {
 		[(id * 2) + 1, (id * 2) + 2]
 	}
 
+	/// Get the id of a blocks parent.
+	/// # Panics
+	/// If called with id = 0, which doesn't have a parent, it will panic.
 	fn get_parent_id(id: usize) -> usize {
 		(id - 1) / 2
 	}
@@ -170,6 +197,8 @@ impl BuddyAllocator {
 		}
 	}
 
+	/// Take a block of a given id. This will remove it from the linked list, by relinking the
+	/// previous and next nodes. It will also update the [BuddyAllocator::xor_free] array.
 	unsafe fn remove_block(&mut self, id: usize) {
 		serial_println!("REMOVE {}", id);
 
@@ -190,13 +219,16 @@ impl BuddyAllocator {
 				}
 			}
 			None => {
-				serial_println!("{}", id);
 				// This should never happen.
 				unreachable!("Tried to remove first node, this shouldn't happen");
 			}
 		}
 	}
 
+	/// Add a free block of a certain id into the proper linked list. It will push it to the front
+	/// of the list, and update the [BuddyAllocator::xor_free] array. If ```with_merge``` is true,
+	/// it will attempt to merge it with its buddy, and if it can, recursively call itself on their
+	/// combined parent block.
 	fn add_free_block(&mut self, id: usize, with_merge: bool) {
 		serial_println!("ADD FREE {}", id);
 		// Check if it's buddy is free
@@ -247,8 +279,10 @@ unsafe impl GlobalAlloc for Locked<BuddyAllocator> {
 	}
 }
 
+/// The error handler for allocation errors. Called automatically by rust. It just prints the
+/// requested layout that caused the allocation fail.
 #[alloc_error_handler]
-fn alloc_error_handler(layout: alloc::alloc::Layout) -> ! {
+fn alloc_error_handler(layout: Layout) -> ! {
 	panic!("allocation error: {:?}", layout)
 }
 

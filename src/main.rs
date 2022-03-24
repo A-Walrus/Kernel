c8a109d0d135de1cba56ad8cd1c96a8bcb7e1f59
Awalrus <ofek.r.guy@gmail.com>
2022-03-24 12:27:15 +0200

    Initial attempt, (from osdev)
    It doesn't work, crashes instantly

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![no_std]
 #![no_main]
+#![feature(asm)]
 
 extern crate alloc;
 use bootloader::{entry_point, BootInfo};
@@ -12,6 +13,11 @@ use kernel::{
 };
 
 entry_point!(kernel_main);
+
+fn test_function() {
+	// serial_println!("Test function");
+	loop {}
+}
 
 /// Entry point for the kernel. Returns [!] because it is never supposed to exit.
 fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
@@ -28,11 +34,57 @@ fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
 		unsafe {
 			buffer::TERM = Some(term);
 		}
-		ext2::setup().expect("Failed to setup EXT2");
+		// ext2::setup().expect("Failed to setup EXT2");
 		serial_println!("Finished setup");
+
 		// TODO do stuff here
 
-		ext2::cleanup().expect("Failed to cleanup EXT2");
+		unsafe {
+			// sysret method
+			// Taken from osdev, changed ecx, to rcx since ecx is 32 bits, not 64
+			asm!(
+				"mov rcx, 0xc0000082",
+				"wrmsr",
+				"mov rcx, 0xc0000080",
+				"wrmsr",
+				"or eax, 1",
+				"wrmsr",
+				"mov rcx, 0xc0000081",
+				"rdmsr",
+				"mov edx, 0x00180008",
+				"wrmsr",
+				"mov rcx, {function}",
+				"mov r11, 0x202",
+				"sysretq",
+				function = in(reg) test_function,
+			);
+
+			// iret method (doesn't compile because 64 bit mode)
+			// asm!(
+			// 	"mov ax, (4 * 8) | 3",
+			// 	"mov ds, ax",
+			// 	"mov es, ax",
+			// 	"mov fs, ax",
+			// 	"mov gs, ax",
+			// 	"mov eax, esp",
+			// 	"push (4 * 8) | 3",
+			// 	"push eax",
+			// 	"pushf",
+			// 	"push (3 * 8) | 3",
+			// 	"push {function}",
+			// 	"iret",
+			// 	function = in(reg) test_function,
+			// );
+
+			// // Testing jumping, and function pointers. Seems to work
+			// asm!(
+			// 	"jmp {function}",
+			// 	function = in(reg) test_function,
+			// );
+		}
+		serial_println!("After inline asm");
+
+		// ext2::cleanup().expect("Failed to cleanup EXT2");
 		serial_println!("Finished cleanup");
 	}
 	serial_println!("The end");

e658c4cc36055fe51393bbc88b33429fed47c996
Awalrus <ofek.r.guy@gmail.com>
2022-04-17 16:55:16 +0300

    Add basic process control structures

--- Kernel/src/process/mod.rs
+++ Kernel/src/process/mod.rs
@@ -1,2 +1,23 @@
+use alloc::vec::Vec;
+
 /// Module for working with elf executables
 pub mod elf;
+
+type Pid = usize;
+
+type OpenFiles = ();
+
+enum State {
+	New,
+	Ready,
+	Suspended,
+}
+
+/// Process control block
+pub struct PCB {
+	pid: Pid,
+	state: State,
+	page_table: (),
+	registers: (),
+	open_files: OpenFiles,
+}

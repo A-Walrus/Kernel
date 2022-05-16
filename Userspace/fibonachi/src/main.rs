6d649bc159c1c1e1758e428d75330616625f13fc
Awalrus <ofek.r.guy@gmail.com>
2022-05-16 09:43:06 +0300

    Make fibonachi accept arguement

--- Userspace/fibonachi/src/main.rs
+++ Userspace/fibonachi/src/main.rs
@@ -2,17 +2,25 @@
 #![no_std]
 
 use alloc::{format, vec::Vec};
-use standard::{io::Write, print, println, syscalls::File};
+use standard::{get_args, io::Write, print, println, syscalls::File};
 extern crate alloc;
 
 #[no_mangle]
 pub extern "C" fn main() -> isize {
+	let args = get_args();
+
+	let n: usize = match args.get(0).map(|s| s.parse()) {
+		None => 10,
+		Some(Ok(num)) => num,
+		Some(Err(_)) => {
+			println!("Arguement must be a number!");
+			return -1;
+		}
+	};
+
 	let mut a: u64 = 0;
 	let mut b: u64 = 1;
 
-	let n = 100;
-
-	// syscalls::print(&format!("First {} Fibonachi numbers:\n", n));
 	println!("First {} Fibonachi numbers:", n);
 
 	let mut v = Vec::new();

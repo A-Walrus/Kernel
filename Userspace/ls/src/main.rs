143c5e2d02254e5bfa923fc614912bd8244a999d
Awalrus <ofek.r.guy@gmail.com>
2022-05-18 13:31:58 +0300

    Improve ls ux

--- Userspace/ls/src/main.rs
+++ Userspace/ls/src/main.rs
@@ -3,15 +3,26 @@
 
 use standard::{get_args, println, syscalls::Dir};
 extern crate alloc;
+use alloc::string::String;
 
 #[no_mangle]
 pub extern "C" fn main() -> isize {
 	let args = get_args();
 
 	let path = args.get(0).unwrap_or(&"/");
-	let dir = Dir::open(path).unwrap();
-	for entry in dir {
-		println!("{}", entry);
+	match Dir::open(path) {
+		Ok(dir) => {
+			let mut string = String::new();
+			for entry in dir {
+				string.push_str(&entry);
+				string.push_str("  ");
+			}
+			println!("{}", string);
+			return 0;
+		}
+		Err(_) => {
+			println!("Failed to open directory: {}", path);
+			return -1;
+		}
 	}
-	return 0;
 }

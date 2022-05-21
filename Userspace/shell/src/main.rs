912e518f58ddf99f9f1e8859d395d603cef8ee2a
Awalrus <ofek.r.guy@gmail.com>
2022-05-21 11:59:27 +0300

    Start work on lexer with logos crate

--- Userspace/shell/src/main.rs
+++ Userspace/shell/src/main.rs
@@ -8,31 +8,54 @@ use standard::{
 	syscalls::{exec, file_exists, read_line, wait},
 };
 
+use logos::{Lexer, Logos};
+
+#[derive(Logos, Debug, PartialEq)]
+enum Token {
+	#[token("&")]
+	Asynchronous,
+
+	#[regex("[a-zA-Z]+")]
+	#[regex("\"[a-zA-Z]+\"")]
+	#[regex("'[a-zA-Z]+'")]
+	Text,
+
+	#[error]
+	Error,
+
+	#[regex(r"[ \t\n\f]+", logos::skip)]
+	WhiteSpace,
+}
+
 #[no_mangle]
 pub extern "C" fn main() -> isize {
 	loop {
 		print!("GuyOS > ");
 		let input = read_line();
-		let mut split = input.split_ascii_whitespace();
-		match split.next() {
-			Some("exit") => {
+		match input.as_str() {
+			"" => {}
+			"exit" => {
 				break;
 			}
-			Some(exec_path) => {
-				let args: Vec<&str> = split.collect();
-				let mut path = "/bin/".to_string();
-				path.push_str(exec_path);
-				if file_exists(&path) {
-					let pid = exec(&path, &args);
-					match pid {
-						Ok(pid) => wait(pid),
-						_ => {}
-					}
-				} else {
-					println!("{} does not exist", path);
+			line => {
+				let lex = Token::lexer(line);
+				for item in lex {
+					println!("{:?}", item);
 				}
+
+				// let args: Vec<&str> = split.collect();
+				// let mut path = "/bin/".to_string();
+				// path.push_str(line);
+				// if file_exists(&path) {
+				// 	let pid = exec(&path, &args);
+				// 	match pid {
+				// 		Ok(pid) => wait(pid),
+				// 		_ => {}
+				// 	}
+				// } else {
+				// 	println!("{} does not exist", path);
+				// }
 			}
-			None => {}
 		}
 	}
 	return 0;

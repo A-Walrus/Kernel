d59ba205b889820e4a3e7235ec6220d6bf8c14cf
Awalrus <ofek.r.guy@gmail.com>
2022-05-27 19:17:40 +0300

    Add mv application

---
+++ Userspace/mv/src/main.rs
@@ -0,0 +1,47 @@
+#![no_main]
+#![no_std]
+
+use standard::{
+	io::{Read, Write},
+	syscalls::*,
+	*,
+};
+extern crate alloc;
+use alloc::vec::Vec;
+
+#[no_mangle]
+pub extern "C" fn main() -> isize {
+	let args = get_args();
+
+	if args.len() == 2 {
+		let source = args[0];
+		let dest = args[1];
+
+		match File::open(source) {
+			Ok(mut file) => {
+				let mut buf = Vec::new();
+				file.read_to_end(&mut buf).expect("Failed to read!");
+
+				match File::create(dest) {
+					Ok(mut file) => {
+						file.write(&buf).expect("Failed to write!");
+						unlink(source).expect("Failed to delte source!");
+
+						return 0;
+					}
+					Err(_) => {
+						println!("Failed to create file!");
+						return 0;
+					}
+				}
+			}
+			Err(_) => {
+				println!("Failed to open file!");
+				return 0;
+			}
+		}
+	} else {
+		println!("Incorrect numberr of args");
+		return -1;
+	}
+}
